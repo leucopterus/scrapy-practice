bcfa91239dc58aa5f3ead8661cfcd2f8845b9de8
leucopterus <strelchenkoedgar@gmail.com>
2020-06-21 16:27:03 +0300

    changes in settings, middleware and scrapper

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -22,12 +22,12 @@ NEWSPIDER_MODULE = 'tutorial.spiders'
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 64
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 1
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -88,3 +88,10 @@ ROBOTSTXT_OBEY = False
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+RETRY_HTTP_CODES = [429]
+
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy.downloadermiddlewares.retry.RetryMiddleware': None,
+    'tutorial.middlewares.TooManyRequestsRetryMiddleware': 543,
+}
--- tutorial/tutorial/spiders/github_spider.py
+++ tutorial/tutorial/spiders/github_spider.py
@@ -11,23 +11,35 @@ class GithubSpider(scrapy.Spider):
     limit = 11
 
     def parse(self, response):
-        repos_urls = response.\
-                css('ul.repo-list').css('li.public').css('div.f4.text-normal')\
-                .css('a').xpath('@href').getall()
         next_page_url = response.css('a.next_page').xpath('@href').get()
         [next_page_number] = [int(item.split('=')[1])
                               for item in next_page_url.split('?')[1].split('&')
                               if item.startswith('p=')]
 
         if next_page_number <= self.limit:
-            yield from response.follow_all(repos_urls, self.parse_repos, cb_kwargs=dict(page=next_page_number-1))
+            repos_urls = response. \
+                css('ul.repo-list li.public.source div.mt-n1 div.f4.text-normal a').xpath('@href').getall()
+            for repo_url in repos_urls:
+                # yield {
+                #     f'{next_page_number-1}': f'{repo_url}'
+                # }
+                yield response.follow(repo_url, self.parse_repos, cb_kwargs=dict(page=next_page_number - 1))
+
             yield response.follow(next_page_url, callback=self.parse)
+            # for repo_url in repos_urls:
+            #     yield response.follow(repo_url, self.parse_repos, cb_kwargs=dict(page=next_page_number-1))
+            #
+            # yield response.follow(next_page_url, callback=self.parse)
 
     def parse_repos(self, response, page):
-        repos_name = response.url.split('github.com/')[1]
-        repos_last_commit = response.css('a.commit-tease-sha.mr-1::text').get().strip()
-        yield {
-            'page': page,
-            'repo': repos_name,
-            'last_commit': repos_last_commit,
-        }
+        if 'Cannot retrieve the latest commit at this time' not in str(response.body):
+            repos_name = response.url.split('github.com/')[1]
+            repos_last_commit = response.css('a.commit-tease-sha.mr-1::text').get()
+            repos_last_commit = repos_last_commit.strip() if isinstance(repos_last_commit, str) else None
+            yield {
+                'page': page,
+                'repo': repos_name,
+                'last_commit': repos_last_commit,
+            }
+        else:
+            yield response.follow(response.url, self.parse_repos, cb_kwargs=dict(page=page))
