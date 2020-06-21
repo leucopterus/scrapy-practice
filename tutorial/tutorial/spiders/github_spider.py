babb134d5c757c300c2a3c347825f8e1942750a0
leucopterus <strelchenkoedgar@gmail.com>
2020-06-21 20:38:58 +0300

    some changes in scrapper class

--- tutorial/tutorial/spiders/github_spider.py
+++ tutorial/tutorial/spiders/github_spider.py
@@ -1,3 +1,5 @@
+import copy
+
 import scrapy
 
 
@@ -8,38 +10,39 @@ class GithubSpider(scrapy.Spider):
         'https://github.com/search?p=1&q=python&type=Repositories'
     ]
 
-    limit = 11
+    def __init__(self, limit=2, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.limit = limit + 1
+        self.next_page_number = None
+        self.repos_urls = []
+        self.repos_data_mask = {'link number': None, 'repo': None, 'last commit': None}
 
     def parse(self, response):
         next_page_url = response.css('a.next_page').xpath('@href').get()
-        [next_page_number] = [int(item.split('=')[1])
-                              for item in next_page_url.split('?')[1].split('&')
-                              if item.startswith('p=')]
+        [self.next_page_number] = [int(item.split('=')[1])
+                                   for item in next_page_url.split('?')[1].split('&')
+                                   if item.startswith('p=')]
 
-        if next_page_number <= self.limit:
+        if self.next_page_number <= self.limit:
             repos_urls = response. \
                 css('ul.repo-list li.public.source div.mt-n1 div.f4.text-normal a').xpath('@href').getall()
             for repo_url in repos_urls:
-                # yield {
-                #     f'{next_page_number-1}': f'{repo_url}'
-                # }
-                yield response.follow(repo_url, self.parse_repos, cb_kwargs=dict(page=next_page_number - 1))
+                self.repos_urls.append(''.join(['https://github.com/', repo_url]))
 
             yield response.follow(next_page_url, callback=self.parse)
-            # for repo_url in repos_urls:
-            #     yield response.follow(repo_url, self.parse_repos, cb_kwargs=dict(page=next_page_number-1))
-            #
-            # yield response.follow(next_page_url, callback=self.parse)
-
-    def parse_repos(self, response, page):
-        if 'Cannot retrieve the latest commit at this time' not in str(response.body):
-            repos_name = response.url.split('github.com/')[1]
-            repos_last_commit = response.css('a.commit-tease-sha.mr-1::text').get()
-            repos_last_commit = repos_last_commit.strip() if isinstance(repos_last_commit, str) else None
-            yield {
-                'page': page,
-                'repo': repos_name,
-                'last_commit': repos_last_commit,
-            }
         else:
-            yield response.follow(response.url, self.parse_repos, cb_kwargs=dict(page=page))
+            for link_number, repo_url in enumerate(self.repos_urls, start=1):
+                yield response.follow(repo_url, callback=self.parse_repos, cb_kwargs=dict(link_number=link_number))
+
+    def parse_repos(self, response, link_number):
+        if 'Cannot retrieve the latest commit at this time' not in str(response.body):
+            repos_data = copy.copy(self.repos_data_mask)
+
+            repos_last_commit = response.css('a.commit-tease-sha.mr-1::text').get()
+
+            repos_data['link number'] = link_number
+            repos_data['repo'] = response.url.split('github.com/')[1]
+            repos_data['last commit'] = repos_last_commit.strip() if isinstance(repos_last_commit, str) else None
+            yield repos_data
+        else:
+            yield response.follow(response.url, self.parse_repos, cb_kwargs=dict(link_number=link_number))
