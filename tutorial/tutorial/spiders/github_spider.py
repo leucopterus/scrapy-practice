90c0ccaea3de331d00eb7922e7e37f41d7e8b9b8
leucopterus <strelchenkoedgar@gmail.com>
2020-06-23 14:45:20 +0300

    sleep time, with-as and gc.collect are added, settings and middleware were changed

--- tutorial/tutorial/spiders/github_spider.py
+++ tutorial/tutorial/spiders/github_spider.py
@@ -1,4 +1,6 @@
 import copy
+import gc
+import json
 import time
 
 import scrapy
@@ -11,11 +13,13 @@ class GithubSpider(scrapy.Spider):
         'https://github.com/search?p=1&q=python&type=Repositories'
     ]
 
-    def __init__(self, start=1, limit=10, *args, **kwargs):
+    def __init__(self, start=1, limit=10, lists=True, items=True, *args, **kwargs):
         super().__init__(*args, **kwargs)
         start, limit = int(start), int(limit)
         self.start = start
         self.limit = limit + start - 1
+        self.print_list = bool(json.loads(str(lists).lower()))
+        self.print_item = bool(json.loads(str(items).lower()))
         self.next_page_number = None
         self.repos_data_mask = {
             'page': None,
@@ -30,7 +34,6 @@ class GithubSpider(scrapy.Spider):
             self.start_urls[0] = url
 
     def parse(self, response):
-        self._controller(10)
         next_page_url = response.css('a.next_page').xpath('@href').get()
         if next_page_url is not None:
             [self.next_page_number] = [int(item.split('=')[1])
@@ -42,18 +45,28 @@ class GithubSpider(scrapy.Spider):
         repos_urls = response. \
             css('ul.repo-list li.public.source div.mt-n1 div.f4.text-normal a').\
             xpath('@href').getall()
+
+        if self.print_list:
+            with open(f'./output/links-page{self.next_page_number-1}.json', 'w') as links_json:
+                links_json.write(json.dumps(dict(data=repos_urls)))
+
         for link_number, repo_url in enumerate(repos_urls, start=1):
             yield response.follow(repo_url,
                                   callback=self.parse_repos,
+                                  dont_filter=True,
                                   cb_kwargs=dict(
                                       page_number=self.next_page_number-1,
                                       link_number=link_number,
                                   ))
 
+        self._controller_sleep(150)
+        gc.collect()
+
         if self.next_page_number <= self.limit:
             yield response.follow(next_page_url, callback=self.parse)
 
     def parse_repos(self, response, page_number, link_number):
+        repo = response.url.split('github.com/')[1]
         if 'Cannot retrieve the latest commit at this time' not in response.text:
             repos_data = copy.copy(self.repos_data_mask)
 
@@ -61,29 +74,40 @@ class GithubSpider(scrapy.Spider):
 
             repos_data['page'] = page_number
             repos_data['link'] = link_number
-            repos_data['repo'] = response.url.split('github.com/')[1]
+            repos_data['repo'] = repo
             repos_data['commit'] = repos_last_commit.strip() if isinstance(repos_last_commit, str) else None
-            return repos_data
+
+            if self.print_item:
+                with open(f'./output/page{page_number}link{link_number}.json', 'w') as json_doc:
+                    json_doc.write(json.dumps(repos_data))
+
+            yield repos_data
+
         else:
             page_with_commits_url = response.css('ul.numbers-summary li.commits a').xpath('@href').get()
-            self._controller(10)
-            return response.follow(page_with_commits_url,
-                                   callback=self.parse_commits,
-                                   cb_kwargs=dict(
-                                       page_number=page_number,
-                                       link_number=link_number,
-                                   ))
+            yield response.follow(page_with_commits_url,
+                                  callback=self.parse_commits,
+                                  cb_kwargs=dict(
+                                      page_number=page_number,
+                                      link_number=link_number,
+                                      repo=repo,
+                                  ))
 
-    def parse_commits(self, response, page_number, link_number):
+    def parse_commits(self, response, page_number, link_number, repo):
         repos_last_commit = response.css('ol.commit-group.Box li.commit div.commit-links-group a.sha::text').get()
         repos_data = copy.copy(self.repos_data_mask)
         repos_data['page'] = page_number
         repos_data['link'] = link_number
-        repos_data['repo'] = response.url.split('github.com/')[1]
+        repos_data['repo'] = repo
         repos_data['commit'] = repos_last_commit.strip() if isinstance(repos_last_commit, str) else None
-        return repos_data
 
-    def _controller(self, seconds=30):
+        if self.print_item:
+            with open(f'./output/page{page_number}link{link_number}.json', 'w') as json_doc:
+                json_doc.write(json.dumps(repos_data))
+
+        yield repos_data
+
+    def _controller_sleep(self, seconds=30):
         self.crawler.engine.pause()
         time.sleep(seconds)
         self.crawler.engine.unpause()
